443b912aa1145e8663e02d502f7c570402315965
Ken <knetngyn@gmail.com>
2022-10-28 22:12:51 -0500

    Finish designing Cart page

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -37,29 +37,65 @@ const Cart = () => {
   const renderCart = () => {
     if (cart.length === 0) {
       return (
-        <main className="cart-main">
+        <main>
           <img
             src={require("../assets/angryMayreel.gif")}
             alt="angry mayreel"
           />
-          <div>Cart Empty</div>
+          <div className="mayreel-cart-text">Cart Empty</div>
         </main>
       );
     }
-    return cart.map((item) => (
-      <li key={item.id.id}>
-        <img src={item.id.image} alt={item.id.name} />
-        <div>{item.id.name}</div>
-        <div>qty: {item.qty}</div>
-      </li>
-    ));
+    return (
+      <>
+        <h1>Cart</h1>
+        <ul className="cart-list">
+          {cart.map((item) => (
+            <li key={item.id.id}>
+              <img src={item.id.image} alt={item.id.name} />
+              <div className="cart-product-name">{item.id.name}</div>
+              <div className="cart-price-container">
+                <div className="cart-price-info">
+                  <div>Item Price</div>
+                  <div>${item.id.price}.00</div>
+                </div>
+                <div className="cart-price-info">
+                  <div>Quantity</div>
+                  <div>{item.qty}</div>
+                </div>
+
+                <div className="cart-price-info">
+                  <div>Total Price</div>
+                  <div>${item.qty}.00</div>
+                </div>
+              </div>
+              <div className="cart-buttons-container">
+                <button className="cart-remove-button">Remove</button>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <div className="checkout-container">
+          <div className="checkout-title">Order Summary</div>
+          <div className="sub-check-container">
+            <div>Subtotal</div>
+            <div>all ur money</div>
+          </div>
+          <div className="sub-check-container">
+            <div>Tax</div>
+            <div>gimme ur wallet</div>
+          </div>
+          <div className="sub-check-container estimate-container">
+            <div>Estimated Total</div>
+            <div>USD $fart</div>
+          </div>
+          <button className="checkout-button">Checkout</button>
+        </div>
+      </>
+    );
   };
 
-  return (
-    <div>
-      <ul>{renderCart()}</ul>
-    </div>
-  );
+  return <main className="cart-main">{renderCart()}</main>;
 };
 
 export { addProduct, Cart, totalQty };
